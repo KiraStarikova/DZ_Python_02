de3b5c6b492c972e945e506a484021b3dfe10015
Kira <starkial@lenta.ru>
2022-10-03 16:20:25 +0500

    Task 5 добавлено перемешивание заданного списка

--- Task5.py
+++ Task5.py
@@ -1,2 +1,11 @@
 '''Реализуйте алгоритм перемешивания списка.'''
 
+
+def shuff(a):
+    import random
+    b = a[:]
+    random.shuffle(b)
+    return b
+
+a = shuff([23, 1.36, 3, -4, 78])
+print (a)
